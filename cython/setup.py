698e22700d23eb332d6682c8fada63000825eedd
Sergey Chernikov <schernikov@gmail.com>
2014-02-27 01:07:50 -0800

    add explicit numpy path

--- cython/setup.py
+++ cython/setup.py
@@ -4,6 +4,7 @@ Created on Jun 27, 2013
 @author: schernikov
 '''
 
+import numpy as np
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
@@ -31,4 +32,4 @@ ext_modules.extend([Extension('minutescoll',
 
 res = setup(ext_modules = ext_modules,
             #cmdclass = {'build_ext': cpp_build_ext},
-            script_args=['build_ext', '--inplace'])
+            script_args=['build_ext', '--inplace'], include_dirs=[np.get_include()])
